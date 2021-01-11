ba3fc87657abf7fec56b49a3f2910f28e4c0011b
Rafael Alves <alvesrafael17@hotmail.com>
2021-01-11 16:00:33 -0300

    login with github

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,19 +1,44 @@
-import React from 'react';
+import { useEffect } from 'react';
 import { AiOutlineGithub } from 'react-icons/ai';
 import { FcGoogle } from 'react-icons/fc';
 import { MdPersonAdd } from 'react-icons/md';
+import 'firebase/auth';
+import firebase from 'firebase/app';
+
 import logo from 'assets/svg/logo.svg';
 import { Container } from './styles';
 import Button from 'components/Button';
 
+var firebaseConfig = {
+  apiKey: 'AIzaSyDcG1KgbjCjD4jkWlLTMBTgfpYRJ8etM6g',
+  authDomain: 'pizza-dex.firebaseapp.com',
+  projectId: 'pizza-dex',
+  storageBucket: 'pizza-dex.appspot.com',
+  messagingSenderId: '367966904656',
+  appId: '1:367966904656:web:fb05246a36159ffcdbcc55',
+  measurementId: 'G-LH705P9R8P',
+};
+
+if (!firebase.apps.length) {
+  firebase.initializeApp(firebaseConfig);
+} else {
+  firebase.app(); // if already initialized, use that one
+}
+
 const Login = () => {
+  const loginWithGitHub = async () => {
+    const provider = new firebase.auth.GithubAuthProvider();
+
+    firebase.auth().signInWithRedirect(provider);
+  };
+
   return (
     <Container>
       <div className="header">
-        <img className="logo" src={logo} />
+        <img className="logo" alt="Logo PizzaDex" src={logo} />
         <h1>Entrar</h1>
       </div>
-      <Button background="white" color="black">
+      <Button onClick={loginWithGitHub} background="white" color="black">
         Entrar com GitHub
         <AiOutlineGithub style={{ marginLeft: 8 }} size={24} />
       </Button>
